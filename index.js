0848118f3a42d85525593dded1082c4f2ad46996
Sofia <sofia.cronstrom@gmail.com>
2023-05-17 16:10:28 +0200

    setting up listener on port 3000 in index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const mongoose = require('mongoose');
 
 //transfer the contents of Express into a new constant called app.
 const app = express();
+
+//listen to changes of this file at port 3000
+app.use(express.json());
+
+app.listen(3000, () =>(
+    console.log(`Server started at${3000}`)
+));
